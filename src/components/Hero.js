27a4502c9c3f85f5101684ec38ac16327f90bdd0
Hunter <fffarmer@portonmail.com>
2022-06-14 08:41:30 -0400

    added new files and hero image

---
+++ src/components/Hero.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Hero.css'
+
+
+export const Hero = () => {
+  return (
+    <div className='hero'>
+        <div className='container'>
+
+            {/* Left side */}
+
+
+            {/* Right side */}
+
+    </div>
+  )
+}
