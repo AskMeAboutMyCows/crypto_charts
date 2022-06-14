298b2932126f868ffc851b3bddc4a0f3be211202
Hunter <fffarmer@portonmail.com>
2022-06-13 21:08:31 -0400

    added on clcik event

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,18 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { FaBars } from 'react-icons/fa'
 import './Navbar.css'
 
 
 const navbar = () => {
+const [click, setclick] = useState(false)
+const handleClick = () => setclick(!click)
+  
+  
   return (
     <div className="header">
       <div className="container">
-        <h1>De<span className="primary"> Fi</span></h1>
+        <h1>De<span className="primary">Fi</span></h1>
+        <ul className={click ? 'nav-menu active' : 'nav-menu'}/>
         <ul className='nav-menu'>
           <li>
             <a href="/">Home</a>
@@ -25,8 +30,9 @@ const navbar = () => {
         <div className='btn-group'>
           <button className='btn'>Connect Wallet</button>
         </div>
-        <div className='hamburger'>
-          <FaBars size={20} style={{color: '#333'}} />
+        <div className='hamburger'> onClick={handleClick}
+        {click ? <FaTimes  size={20} style={{color: '#333'}}/> : (<FaBars size={20} style={{color: '#333'}} />)}
+          
       </div>
     </div>
     </div>
