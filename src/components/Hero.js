5624e00f79633d5dbd46f898d2f12289912396cd
Hunter <fffarmer@portonmail.com>
2022-06-14 08:48:36 -0400

    updated hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,6 +8,15 @@ export const Hero = () => {
         <div className='container'>
 
             {/* Left side */}
+            <div className='left'>
+                <p> Buy & Sell Crypto 24/7 using your retirement account </p>
+                <h1> Invest in Cryptocurrency with your IRA</h1>
+                <p> Buy, sell, and store hundreds of cryptocurrencies with your IRA. </p>
+                <div className='input-container'>
+                    <input type='email' placeholder='Enter your email' />
+                    <button> className='btn'> Get Started </button>.
+                    
+
 
 
             {/* Right side */}
