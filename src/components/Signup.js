a09e10f79678b9b389784beca8d7df08842ae1ca
Hunter <fffarmer@protonmail.com>
2022-06-17 17:37:07 -0400

    added signup.js need to do right side

---
+++ src/components/Signup.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Signup.css'
+
+import Crypto from '../assets/trade.png'
+
+const Signup = () => {
+    return (
+        <div className='signup'>
+            <div className='container'>
+                {/* left */}
+                <div className='left'>
+                    <img src={Crypto} alt='' />
+                </div>
