818c47e6d9c5689810492d1553ce59c90c497c9e
Hunter <fffarmer@protonmail.com>
2022-06-18 21:33:21 -0400

    added imports to app js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import Signup from './components/Signup'
+import Footer from './components/Footer'
 
 function App() {
   return (
